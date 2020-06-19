2b8491408a5adb6b38521109b4cfbaabe720dec6
357157743 <37918761+357157743@users.noreply.github.com>
2020-06-19 17:59:32 +0800

    2020.6.19 netty 学习

---
+++ src/main/java/com/mashibing/nettystudy/s02/ClientFrame.java
@@ -0,0 +1,34 @@
+package com.mashibing.nettystudy.s02;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @date 2020/6/19 - 15:24
+ */
+public class ClientFrame extends Frame {
+    TextArea ta = new TextArea();
+    TextField tf  = new TextField();
+
+    public ClientFrame(){
+        this.setSize(600,400);
+        this.setLocation(100,20);
+        this.add(ta,BorderLayout.CENTER);
+        this.add(tf,BorderLayout.SOUTH);
+
+        tf.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ta.setText(ta.getText() + tf.getText());
+                tf.setText("");
+            }
+        });
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ClientFrame();
+
+    }
+}
