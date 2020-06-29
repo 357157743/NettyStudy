96477c81ab28315e871628eb902a997206770d96
357157743 <37918761+357157743@users.noreply.github.com>
2020-06-29 17:36:08 +0800

    2020.6.29 netty12 学习

---
+++ src/main/java/com/mashibing/nettystudy/s10/ServerFrame.java
@@ -0,0 +1,44 @@
+package com.mashibing.nettystudy.s10;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @date 2020/6/19 - 15:24
+ */
+public class ServerFrame extends Frame {
+
+    Button btnStart = new Button("start");
+    TextArea taLeft = new TextArea();
+    TextField taRight  = new TextField();
+
+
+    public ServerFrame(){
+        this.setSize(1600,400);
+        this.setLocation(300,30);
+        this.add(btnStart,BorderLayout.NORTH);
+        Panel p = new Panel(new GridLayout(1,2));
+        p.add(taLeft);
+        p.add(taRight);
+
+        this.add(p);
+
+
+        //客户端点红色叉子 关闭
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+      new Server();
+    }
+}
---
+++ src/main/java/com/mashibing/nettystudy/s11/ClientFrame.java
@@ -0,0 +1,62 @@
+package com.mashibing.nettystudy.s11;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @date 2020/6/19 - 15:24
+ */
+public class ClientFrame extends Frame {
+
+    public static final ClientFrame INSTANCE = new ClientFrame();
+
+    TextArea ta = new TextArea();
+    TextField tf  = new TextField();
+
+    Client c= null;
+
+    public ClientFrame(){
+        this.setSize(600,400);
+        this.setLocation(100,20);
+        this.add(ta,BorderLayout.CENTER);
+        this.add(tf,BorderLayout.SOUTH);
+
+        tf.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                c.send(tf.getText()); // 把字符串发送到服务器
+                //ta.setText(ta.getText() + tf.getText());
+                tf.setText("");
+            }
+        });
+
+        //客户端点红色叉子 关闭
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                c.closeConnect();
+                System.exit(0);
+            }
+        });
+
+    }
+
+    private void connectToServer() {
+        c= new Client();
+        c.connect();
+    }
+
+    public static void main(String[] args) {
+       ClientFrame frame = ClientFrame.INSTANCE;
+       frame.setVisible(true);
+       frame.connectToServer();
+
+    }
+
+    public void updateText(String msgAccepted) {
+        this.ta.setText(ta.getText() + System.getProperty("line.separator") + msgAccepted);
+    }
+}
